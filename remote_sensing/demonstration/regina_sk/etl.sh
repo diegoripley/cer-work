c45abc788c32a09a934b23d0f4ca58197c7a923a
Diego Ripley <diego@diegoripley.ca>
2020-12-04 06:20:45 -0500

    Updates

--- remote_sensing/demonstration/regina_sk/etl.sh
+++ remote_sensing/demonstration/regina_sk/etl.sh
@@ -1,44 +1,39 @@
 #!/bin/sh
 
-# DSM
-for file in dsm_*.tif
-do
-  echo $file
-  gdalwarp -t_srs EPSG:26913 \
-       -co "COMPRESS=LZW" \
-       $file out/$file
-done
+# Merge
+# -co TILED=YES
 
 # DTM
-for file in dtm_*.tif
-do
-  echo $file
-  gdalwarp -t_srs EPSG:26913 \
-       -co "COMPRESS=LZW" \
-       $file out/$file
-done
-
-# Merge
-gdal_merge.py -o dsm_1m_utm13_e_regina.tif -ot Float32 \
-        -n -32767 \
-        -a_nodata -32767 \
-        -co NUM_THREADS=ALL_CPUS -co COMPRESS=LZW -co TILED=YES out/dsm_*.tif
-
 gdal_merge.py -o dtm_1m_utm13_e_regina.tif -ot Float32 \
         -n -32767 \
         -a_nodata -32767 \
-        -co NUM_THREADS=ALL_CPUS -co COMPRESS=LZW -co TILED=YES out/dtm_*.tif
+        -co COMPRESS=LZW dtm_*_e_*_*.tif
+# DSM
+gdal_merge.py -o dsm_1m_utm13_e_regina.tif -ot Float32 \
+        -n -32767 \
+        -a_nodata -32767 \
+        -co COMPRESS=LZW  dsm_*_e_*_*.tif
 
 # Canopy height model ()
 gdal_calc.py -A dtm_1m_utm13_e_regina.tif -B dsm_1m_utm13_e_regina.tif --calc="B - A" \
     --type=Float32 \
     --outfile chm_1m_utm13_regina_1.tif 
+    #-co "COMPRESS=LZW"
 
-gdal_calc.py -A chm_1m_utm13_regina_1.tif  --calc="A*(A>0)" --NoDataValue=0 \
-    --type=Float32 \
-    --outfile chm_1m_utm13_regina.tif --co COMPRESS=LZW --co TILED=YES
+gdal_calc.py -A chm_1m_utm13_regina_1.tif --outfile chm_1m_utm13_regina.tif \
+  --calc="A*(A>0)" --NoDataValue=0 \
+  --type=Float32 --co COMPRESS=LZW
 
+#gdalwarp -t_srs EPSG:26913 \
+#       -co "COMPRESS=LZW" \
+#       dsm_1m_utm13_e_regina_1.tif dsm_1m_utm13_e_regina.tif
+
+#gdalwarp -t_srs EPSG:26913 \
+#       -co "COMPRESS=LZW" \
+#       dtm_1m_utm13_e_regina_1.tif dtm_1m_utm13_e_regina.tif
 # Applying a z factor
 
 https://pro.arcgis.com/en/pro-app/tool-reference/3d-analyst/applying-a-z-factor.htm
 # If x, y units
+# https://docs.qgis.org/testing/en/docs/user_manual/working_with_raster/raster_analysis.html#raster-calculator
+("elevation@1" < 0) * 1
