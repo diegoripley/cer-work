83f60e2cac61af49f7839c7fab137e8cf06f6a73
Diego Ripley <diego@diegoripley.ca>
2020-12-04 04:49:50 -0500

    Good looking maps

--- remote_sensing/demonstration/regina_sk/etl.sh
+++ remote_sensing/demonstration/regina_sk/etl.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# DSM
 for file in dsm_*.tif
 do
   echo $file
@@ -8,21 +9,34 @@ do
        $file out/$file
 done
 
-gdal_merge.py -o regina_dsm.tif -ot Float32 \
+# DTM
+for file in dtm_*.tif
+do
+  echo $file
+  gdalwarp -t_srs EPSG:26913 \
+       -co "COMPRESS=LZW" \
+       $file out/$file
+done
+
+# Merge
+gdal_merge.py -o dsm_1m_utm13_e_regina.tif -ot Float32 \
         -n -32767 \
         -a_nodata -32767 \
         -co NUM_THREADS=ALL_CPUS -co COMPRESS=LZW -co TILED=YES out/dsm_*.tif
 
+gdal_merge.py -o dtm_1m_utm13_e_regina.tif -ot Float32 \
+        -n -32767 \
+        -a_nodata -32767 \
+        -co NUM_THREADS=ALL_CPUS -co COMPRESS=LZW -co TILED=YES out/dtm_*.tif
+
 # Canopy height model ()
-gdal_calc.py -A dtm_1m_utm13_regina.tif -B regina_dsm.tif --calc="B - A" \
+gdal_calc.py -A dtm_1m_utm13_e_regina.tif -B dsm_1m_utm13_e_regina.tif --calc="B - A" \
     --type=Float32 \
     --outfile chm_1m_utm13_regina_1.tif 
 
-gdal_calc.py -A chm_1m_utm13_regina_1.tif  --calc="(A<0)*1" \
-    --type=Float32 -co COMPRESS=LZW -co TILED=YES \
-    --outfile chm_1m_utm13_regina.tif 
-
-
+gdal_calc.py -A chm_1m_utm13_regina_1.tif  --calc="A*(A>0)" --NoDataValue=0 \
+    --type=Float32 \
+    --outfile chm_1m_utm13_regina.tif --co COMPRESS=LZW --co TILED=YES
 
 # Applying a z factor
 
