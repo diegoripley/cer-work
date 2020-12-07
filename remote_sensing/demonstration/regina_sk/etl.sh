8838490cd62eb1d5ff9395497c56ed77a17f4287
Diego Ripley <diego@diegoripley.ca>
2020-12-07 09:52:35 -0500

    Improvements.
    Moved all data to file geodatabase, ArcGIS Pro has issues with reading data from a GeoPackage file.

--- remote_sensing/demonstration/regina_sk/etl.sh
+++ remote_sensing/demonstration/regina_sk/etl.sh
@@ -23,6 +23,12 @@ gdalwarp -t_srs EPSG:26913 \
        -co "COMPRESS=LZW" \
        dsm_1m_utm13_e_regina_1.tif dsm_1m_utm13_e_regina.tif
 
+# Jasper
+gdal_merge.py -o dsm_1m_utm11_e_jasper_1.tif -ot Float32 \
+        -n -32767 \
+        -a_nodata -32767 \
+        -co COMPRESS=LZW  *.tif
+
 
 # Canopy height model ()
 gdal_calc.py -A dtm_1m_utm13_e_regina.tif -B dsm_1m_utm13_e_regina.tif --calc="B-A" \
@@ -30,8 +36,9 @@ gdal_calc.py -A dtm_1m_utm13_e_regina.tif -B dsm_1m_utm13_e_regina.tif --calc="B
     --outfile chm_1m_utm13_regina_1.tif \
     --NoDataValue=-32767
 
-# Where 
-gdal_calc.py -A chm_1m_utm13_regina_1.tif --calc="A*(A>0)" \
+# Where
+# Greater than 1 meter, gets rid of a lot of noise
+gdal_calc.py -A chm_1m_utm13_regina_1.tif --calc="A*(A>1)" \
   --type=Float32 \
   --outfile chm_1m_utm13_regina_2.tif \
   --NoDataValue=-32767
@@ -42,21 +49,15 @@ gdalwarp -t_srs EPSG:26913 \
        -co "PREDICTOR=3"
 
 
+# Then I did the following
+1. Run zonal statistics. Use raster layer (chm). Calculate mean statistics
+2. Run Set Z value for the new building layer
 
 
-#gdal_calc.py -A chm_1m_utm13_regina_1.tif --outfile chm_1m_utm13_regina_2.tif \
-#  --calc="0*(A<0)" \
-#  --type=Float32
+# For linking to trees
+1. Use sample raster values. Remove any that are linked to 0
+2. Set Z value or Esri will not like
 
-#--NoDataValue=0 \
-
-
-#gdalwarp -t_srs EPSG:26913 \
-#       -co "COMPRESS=LZW" \
-#       dtm_1m_utm13_e_regina_1.tif dtm_1m_utm13_e_regina.tif
-# Applying a z factor
-
-https://pro.arcgis.com/en/pro-app/tool-reference/3d-analyst/applying-a-z-factor.htm
-# If x, y units
-# https://docs.qgis.org/testing/en/docs/user_manual/working_with_raster/raster_analysis.html#raster-calculator
-("elevation@1" < 0) * 1
+# Lessons Learned (ArcGIS Pro)
+- Image server significantly slows down export of animation
+- https://resources.esri.ca/arcgis-pro/troubleshooting-arcgis-pro-like-an-esri-canada-support-analyst-part-1
